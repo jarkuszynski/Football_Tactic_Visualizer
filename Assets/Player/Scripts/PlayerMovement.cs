90084faecd19421a7c2eb280b70a6ba1a4dc31ce
jarkuszynski <johnny.arkusz@gmail.com>
2019-01-04 22:04:20 +0100

    Add circle movement

---
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 dist;
+    private float posX;
+    private float posY;
+
+    void OnMouseDown()
+    {
+        dist = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, 9.998f);
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+        gameObject.transform.position = worldPos;
+    }
+}
